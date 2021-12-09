c8610962e7ade8373e23df187fee409e53960129
Eduard Ridilla <eduard.ridilla@student.tuke.sk>
2021-12-09 08:09:09 +0100

    crazy hamburger cleanupoemgalul

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -10,7 +10,7 @@ import mouseManager
 import algorithm
 
 
-class Game():
+class Game:
     def __init__(self):
         self.dev = True
         self.testingMode = False
@@ -38,16 +38,10 @@ class Game():
         self.reloadBoard()
 
         if self.dev:
-            # screenWidth = self.screen.get_width()
-            # screenHeight = self.screen.get_height()
-            # rect = pygame.Rect(screenWidth - 200, screenHeight / 5 + 60 * 3, 100, 40)
-            # self.reloadButton = pygame.draw.rect(self.screen, (255, 255, 255), rect)
-
             print("Level loaded")
             print("Rectangles:", self.level.rectangles)
             print("Statics:", self.level.statics)
             print("Centre points:", self.level.centrePoints)
-            # print("Connections:", self.connections)
 
     def removeTile(self, tile):
         for array in self.level.statics:
@@ -55,24 +49,6 @@ class Game():
                 return
 
         newPoints = copy.copy(self.points)
-        # newConnections = copy.copy(self.connections)
-        #
-        # for i, connection in enumerate(self.connections):
-        #     if self.dev: print("0:", connection[0], "1:", connection[1])
-        #
-        #     if connection[0] == tile:
-        #         index = newConnections.index(connection)
-        #         del newConnections[index]
-        #         self.reloadBoard()
-        #
-        #         if self.dev: print("Deleted", connection)
-        #     if connection[1] == tile:
-        #         index = newConnections.index(connection)
-        #         del newConnections[index]
-        #         self.reloadBoard()
-        #         if self.dev: print("Deleted", connection)
-        #
-        # self.connections = newConnections
 
         for i, point in enumerate(self.points):
             index = newPoints.index(point)
@@ -93,62 +69,14 @@ class Game():
         for array in self.points:
             self.graphicsManager.drawEndPoint(array[0], array[1])
 
-        # for array in self.connections:
-        #     self.graphicsManager.drawLine(array[0], array[1], array[2])
-
-        # self.smoothenTurns()
-
         if checker.checkWin(self.level.statics, self.level.rectangles, self.points):
             self.graphicsManager.drawWinScreen()
+
+        pygame.display.update()
 
     def addPoint(self, tile, colour):
         self.points.append((tile, colour))
         self.reloadBoard()
-
-    # def addConnection(self, tile1, tile2, colour):
-    #     connectionsFound = 0
-    #     staticTile = False
-    #     connectionsFound += self.findConnections(tile1)[0]
-    #     connectionsFound += self.findConnections(tile2)[0]
-    #
-    #     if connectionsFound > 1:
-    #         falseConnection = True
-    #     else:
-    #         falseConnection = False
-    #
-    #         for static in self.level.statics:
-    #
-    #             if static[0] == tile1:
-    #
-    #                 if connectionsFound > 0:
-    #                     falseConnection = True
-    #                 else:
-    #                     if static[1] != colour:
-    #                         falseConnection = True
-    #
-    #             if static[0] == tile1 or static[0] == tile2:
-    #                 if static[1] != colour:
-    #                     falseConnection = True
-    #
-    #     if self.dev: print("Connections found:", connectionsFound)
-    #
-    #     if not falseConnection:
-    #         self.connections.append((tile1, tile2, colour))
-    #         self.reloadBoard()
-    #         if self.dev: print("Connections:", self.connections)
-    #     else:
-    #         self.mouseManager.mousePressed = False
-    #
-    # def smoothenTurns(self):
-    #     for connection in self.connections:
-    #         centrePoint1 = self.level.centrePoints[connection[0]]
-    #         centrePoint2 = self.level.centrePoints[connection[1]]
-    #         self.graphicsManager.drawSmoothTurn(centrePoint1, connection[2])
-    #         self.graphicsManager.drawSmoothTurn(centrePoint2, connection[2])
-    #
-    # def replaceConnection(self, tile, colour):
-    #     self.removeTile(tile)
-    #     self.addConnection(self.lastSelectedTile, tile, colour)
 
     def mousePressed(self):
         pos = pygame.mouse.get_pos()
@@ -192,76 +120,6 @@ class Game():
                 if self.selectedColour != [0, 0, 0]:
                     self.addPoint(i, self.selectedColour)
                     return
-
-                # connectionsFound = self.findConnections(i)
-                # if connectionsFound[0] == 1:
-                #     self.mouseManager.mousePressed = True
-                #     if self.dev: print("Mouse pressed on a coloured tile")
-                #
-                #     index = connectionsFound[1][0]
-                #     self.selectedColour = self.connections[index][2]
-                #     if self.dev: print("Selected colour:", self.selectedColour)
-                #
-                #     return
-                # elif connectionsFound[0] == 2:
-                #     if self.dev: print("Connections:", connectionsFound[0])
-                #     self.removeTile(i)
-
-    # def mouseMoved(self):
-    #     pos = pygame.mouse.get_pos()
-    #
-    #     for i, rect in enumerate(self.level.rectangles):
-    #         if rect.collidepoint(pos):
-    #             if i != self.lastSelectedTile:
-    #
-    #                 neighbourTile = False
-    #
-    #                 if i == self.lastSelectedTile + 1:
-    #                     neighbourTile = True
-    #                 elif i == self.lastSelectedTile - 1:
-    #                     neighbourTile = True
-    #                 elif i == self.lastSelectedTile + self.level.width:
-    #                     neighbourTile = True
-    #                 elif i == self.lastSelectedTile - self.level.width:
-    #                     neighbourTile = True
-    #
-    #                 if neighbourTile:
-    #                     connectionsFound = self.findConnections(i)
-    #                     if self.dev: print(f"Connections found: {connectionsFound[0]}")
-    #
-    #                     if connectionsFound[0] > 0:
-    #
-    #                         connectionIndex = connectionsFound[1][0]
-    #                         if self.dev: print(f"Index: {connectionIndex}")
-    #
-    #                         colour = self.connections[connectionIndex][2]
-    #                         if colour != self.selectedColour:
-    #                             if self.dev: print("Replacing connection...")
-    #                             self.replaceConnection(i, self.selectedColour)
-    #                         else:
-    #                             if self.dev: print("Backtracking, removing connection...")
-    #                             self.removeTile(self.lastSelectedTile)
-    #                         self.lastSelectedTile = i
-    #
-    #
-    #                     else:
-    #                         if self.dev: print("Tile changed:", i)
-    #
-    #                         self.addConnection(self.lastSelectedTile, i, self.selectedColour)
-    #                         self.lastSelectedTile = i
-    #
-    #                 return
-
-    # def findConnections(self, tile):
-    #     connectionsFound = 0
-    #     connectionIndex = []
-    #
-    #     for index, connection in enumerate(self.connections):
-    #         if connection[0] == tile or connection[1] == tile:
-    #             connectionsFound += 1
-    #             connectionIndex.append(index)
-    #
-    #     return connectionsFound, connectionIndex
 
 
 if __name__ == "__main__":
